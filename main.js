03b279574fa7e59058e9fc736565ddf2437a50f0
David Mclean <47851457+davupls@users.noreply.github.com>
2023-07-09 11:02:39 -0500

    Side Mobile Menu - add side menu - slide in transition - ui design & typography - javascript functionality

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 let menuBtn = document.querySelector(".menu-btn");
 let menuImage = document.querySelector(".hamburger-menu");
+let mobileNavMenu = document.querySelector(".mobile-nav");
+
 var closedState = true
 
 function toggle() {
     if (closedState == true) {
         closedState = false
+        mobileNavMenu.style.right = "0"
     } else {
         closedState = true
+        mobileNavMenu.style.right = "-70vw"
+        menuImage.src = "assets/images/icon-menu.svg"
     }
 }
 
